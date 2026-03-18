acfa8524b2b9c38c52b1be68674967ac8b8bb1f2
VishwaMalavia <vishwamalavia1201@gmail.com>
2026-03-18 22:07:28 +0530

    Update socket.jsx

--- client/src/pages/socket.jsx
+++ client/src/pages/socket.jsx
@@ -2,4 +2,7 @@ import { io } from 'socket.io-client';
 
 const SOCKET_SERVER = import.meta.env.VITE_SOCKET_URL;
 
-export const socket = io(SOCKET_SERVER);
+export const socket = io(SOCKET_SERVER, {
+  transports: ["websocket"],   // better for render
+  withCredentials: true
+});
